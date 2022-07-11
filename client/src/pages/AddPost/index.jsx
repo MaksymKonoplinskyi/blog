6b4331888fd87c10c30ce4afbfafd47e88a09a60
MaksymKonoplinskyi <gran7777777@gmail.com>
2022-07-11 20:23:54 +0300

    33

--- client/src/pages/AddPost/index.jsx
+++ client/src/pages/AddPost/index.jsx
@@ -8,11 +8,12 @@ import 'easymde/dist/easymde.min.css';
 import styles from './AddPost.module.scss';
 import { useSelector } from 'react-redux';
 import { selectIsAuth } from "../../redux/slices/auth";
-import { useNavigate, Navigate } from 'react-router-dom';
+import { useNavigate, Navigate, useParams } from 'react-router-dom';
 import axios from '../../axios'
 
 
 export const AddPost = () => {
+  const { id } = useParams()
   const navigate = useNavigate()
   const isAuth = useSelector(selectIsAuth)
   const [isLoading, setLoading] = React.useState(false);
@@ -20,8 +21,9 @@ export const AddPost = () => {
   const [title, setTitle] = React.useState('');
   const [tags, setTags] = React.useState('');
   const [imageUrl, setImageUrl] = React.useState('');
-  //const [user, setUser] = React.useState('');
   const inputFileRef = React.useRef(null);
+
+  const isEditing = Boolean(id)
 
   const handleChangeFile = async (event) => {
     try {
@@ -40,6 +42,7 @@ export const AddPost = () => {
   };
 
 
+
   const onChange = React.useCallback((text) => {
     setText(text);
   }, []);
@@ -52,21 +55,36 @@ export const AddPost = () => {
         title,
         text,
         imageUrl,
-        tags,//.split(','),//////////////
-        //user: req.userId,
+        tags,
       }
 
-      const { data } = await axios.post('/posts', fields)
+      const { data } = isEditing
+        ? await axios.patch(`/posts/${id}`, fields)
+        : await axios.post('/posts', fields)
 
-      const id = data._id
+      const _id = isEditing ? id : data._id
 
-      navigate(`/posts/${id}`)
+      navigate(`/posts/${_id}`)
 
     } catch (err) {
       console.warn(err)
       alert('Ошибка при создании статьи')
     }
   }
+
+  React.useEffect(() => {
+    if (id) {
+      axios.get(`/posts/${id}`).then(({ data }) => {
+        setTitle(data.title)
+        setText(data.text)
+        setImageUrl(data.imageUrl)
+        setTags(data.tags.join(','))
+      }).catch(err => {
+        console.warn(err)
+        alert('Ошибка при изменении статьи')
+      })
+    }
+  }, [])
 
   const options = React.useMemo(
     () => ({
@@ -123,7 +141,7 @@ export const AddPost = () => {
       <SimpleMDE className={styles.editor} value={text} onChange={onChange} options={options} />
       <div className={styles.buttons}>
         <Button onClick={onSubmit} size="large" variant="contained">
-          Опубликовать
+          {isEditing ? 'Сохранить' : 'Опубликовать'}
         </Button>
         <a href="/">
           <Button size="large">Отмена</Button>
