cf712ced5dda0788b3bc2032f6aba8a717fba23f
MaksymKonoplinskyi <gran7777777@gmail.com>
2022-07-20 12:09:35 +0300

    2

--- client/src/components/AddComment/index.jsx
+++ client/src/components/AddComment/index.jsx
@@ -5,15 +5,18 @@ import styles from "./AddComment.module.scss";
 import TextField from "@mui/material/TextField";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
+import { useSelector } from "react-redux";
 
 export const Index = () => {
+  const curentUserData = useSelector(state => state.auth.curentUserData)
+  console.log(curentUserData);
   return (
     <>
       <div className={styles.root}>
-        <Avatar
-          classes={{ root: styles.avatar }}
-          src="https://mui.com/static/images/avatar/5.jpg"
-        />
+      { curentUserData && <Avatar
+        classes={{ root: styles.avatar }}
+        src={`http://localhost:4444${curentUserData.avatarUrl}`}
+      />}
         <div className={styles.form}>
           <TextField
             label="Написать комментарий"
