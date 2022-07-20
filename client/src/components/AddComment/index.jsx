51045d5d9de2ba25a2fb2bc81f727d021eb866af
MaksymKonoplinskyi <gran7777777@gmail.com>
2022-07-20 13:32:29 +0300

    2

--- client/src/components/AddComment/index.jsx
+++ client/src/components/AddComment/index.jsx
@@ -9,24 +9,33 @@ import { useSelector } from "react-redux";
 
 export const Index = () => {
   const curentUserData = useSelector(state => state.auth.curentUserData)
-  console.log(curentUserData);
+  // console.log(curentUserData);
+  const [text, setText] = React.useState('');
+  const onClickSend = async () => {
+    
+    console.log(text);
+    const data = {}//await dispatch(fetchAuth(values))
+  }
   return (
     <>
       <div className={styles.root}>
-      { curentUserData && <Avatar
-        classes={{ root: styles.avatar }}
-        src={`http://localhost:4444${curentUserData.avatarUrl}`}
-      />}
-        <div className={styles.form}>
+        {curentUserData?.avatarUrl && <Avatar
+          classes={{ root: styles.avatar }}
+          src={`http://localhost:4444${curentUserData.avatarUrl}`}
+        />}
+        <form className={styles.form}>
           <TextField
             label="Написать комментарий"
             variant="outlined"
             maxRows={10}
+            type='comentText'
             multiline
             fullWidth
+            value={text}
+            onChange={(e) => setText(e.target.value)}
           />
-          <Button variant="contained">Отправить</Button>
-        </div>
+          <Button onClick={onClickSend} variant="contained">Отправить</Button>
+        </form >
       </div>
     </>
   );
