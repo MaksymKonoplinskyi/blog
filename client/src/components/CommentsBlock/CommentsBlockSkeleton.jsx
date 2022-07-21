7bbd86cfa3b94a26031a1ac03b241468f326e54e
MaksymKonoplinskyi <gran7777777@gmail.com>
2022-07-21 17:10:14 +0300

    4

--- client/src/components/CommentsBlock/CommentsBlockSkeleton.jsx
+++ client/src/components/CommentsBlock/CommentsBlockSkeleton.jsx
@@ -3,8 +3,6 @@ import React from "react";
 import { SideBlock } from "../SideBlock";
 import ListItem from "@mui/material/ListItem";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
-import Avatar from "@mui/material/Avatar";
-import ListItemText from "@mui/material/ListItemText";
 import Divider from "@mui/material/Divider";
 import List from "@mui/material/List";
 import Skeleton from "@mui/material/Skeleton";
@@ -13,27 +11,16 @@ export const CommentsBlock = ({ items, children, isLoading }) => {
   return (
     <SideBlock title="Комментарии">
       <List>
-        {(isLoading ? [...Array(5)] : items).map((obj, index) => (
+        {[...Array(5)].map((obj, index) => (
           <React.Fragment key={index}>
             <ListItem alignItems="flex-start">
               <ListItemAvatar>
-                {isLoading ? (
-                  <Skeleton variant="circular" width={40} height={40} />
-                ) : (
-                  <Avatar alt={obj.user.fullName} src={obj.user.avatarUrl} />
-                )}
+                <Skeleton variant="circular" width={40} height={40} />
               </ListItemAvatar>
-              {isLoading ? (
-                <div style={{ display: "flex", flexDirection: "column" }}>
-                  <Skeleton variant="text" height={25} width={120} />
-                  <Skeleton variant="text" height={18} width={230} />
-                </div>
-              ) : (
-                <ListItemText
-                  primary={obj.user.fullName}
-                  secondary={obj.text}
-                />
-              )}
+              <div style={{ display: "flex", flexDirection: "column" }}>
+                <Skeleton variant="text" height={25} width={120} />
+                <Skeleton variant="text" height={18} width={230} />
+              </div>
             </ListItem>
             <Divider variant="inset" component="li" />
           </React.Fragment>
