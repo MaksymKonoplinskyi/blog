de4177d39550376bc4ba03953143fc849515f0e2
MaksymKonoplinskyi <gran7777777@gmail.com>
2022-07-21 17:22:41 +0300

    7

--- client/src/components/CommentsBlock/CommentsBlock.jsx
+++ client/src/components/CommentsBlock/CommentsBlock.jsx
@@ -7,7 +7,6 @@ import Avatar from "@mui/material/Avatar";
 import ListItemText from "@mui/material/ListItemText";
 import Divider from "@mui/material/Divider";
 import List from "@mui/material/List";
-import Skeleton from "@mui/material/Skeleton";
 
 export const CommentsBlock = ({ items, children, isLoading }) => {
   return (
