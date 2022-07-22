0a75afe257bb2e9bd8a8d7ad4455a37ea8bf3005
MaksymKonoplinskyi <gran7777777@gmail.com>
2022-07-22 14:55:58 +0300

    22

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,4 @@
 export * from "./TagsBlock/TagsBlock";
-export * from "./TagsBlock/Tags";
-export * from "./TagsBlock/TagsSkeleton";
 export * from "./SideBlock/SideBlock";
 export * from "./Header/Header";
 
