9f258faf58c1f13f848834ba1d509c989eb99185
MaksymKonoplinskyi <gran7777777@gmail.com>
2022-07-24 12:42:35 +0300

    d

--- client/src/components/PostInfo/PostCreationInfo.jsx
+++ client/src/components/PostInfo/PostCreationInfo.jsx
@@ -5,7 +5,7 @@ export const PostCreationInfo = ({ avatarUrl, fullName, createdAt }) => {
 const creationDate = createdAt.slice(0,10).split('-').join('.')
 const creationTime = createdAt.slice(11,19)
   return (
-    <div className={styles.root}>
+    <div className={styles.postPostCreationInfo}>
       <img className={styles.avatar} src={`http://localhost:4444${avatarUrl}` || '/noavatar.png'} alt={fullName} />
       <div className={styles.userDetails}>
         <span className={styles.userName}>{fullName}</span>
